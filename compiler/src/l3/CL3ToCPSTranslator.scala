53447c470ca63736c2303b5b29996a6bd60f035f
Will Yu <yconquesty@gmail.com>
2024-03-11 06:12:38 +0100

    a2 in progress

--- compiler/src/l3/CL3ToCPSTranslator.scala
+++ compiler/src/l3/CL3ToCPSTranslator.scala
@@ -27,6 +27,7 @@ object CL3ToCPSTranslator extends (S.Tree => H.Tree) {
         }
         H.LetF(funs, transform(body)(ctx))
       }
+      /*
       case S.App(fun: S.Tree, args: Seq[S.Tree]) =>
         transform(fun)
                  ((a: H.Atom) => {
@@ -41,6 +42,58 @@ object CL3ToCPSTranslator extends (S.Tree => H.Tree) {
                                                            H.LetC(cnts, H.AppF(fun, retC, c +: args)) // Still valid after copied?
                                                          // Case match not exhaustive!
                                                      }))})
+      */
+      case S.App(fun: S.Tree, args: Seq[S.Tree]) =>{
+        //@annotation.tailrec
+        def appTransform(es: Seq[S.Tree])(as:Seq[H.Atom]): H.Tree = {
+          es match {
+            case Seq() => {
+              val ret = Symbol.fresh("ret")
+              val v   = Symbol.fresh("v")
+              H.LetC(Seq(H.Cnt(ret, Seq(v), ctx(v))),
+                     H.AppF(as.head, ret, as.tail))
+            }
+            case e +: es => transform(e)
+                                     ((a: H.Atom) => appTransform(es)(as :+ a))
+          }
+        }
+        appTransform(fun +: args)(Seq.empty[H.Atom])
+      }
+      case S.If(S.Prim(prim: S.Primitive, args: Seq[S.Tree]),
+                thenE: S.Tree, elseE: S.Tree) =>
+        val c = Symbol.fresh("c")
+        val r = Symbol.fresh("r")
+        val thenC = Symbol.fresh("thenC")
+        val elseC = Symbol.fresh("elseC")
+        H.LetC(Seq(H.Cnt(c, Seq(r), ctx(r)),
+                   H.Cnt(thenC, Seq.empty[Symbol], transform(thenE)((a: H.Atom) => H.AppC(c, Seq(a)))),
+                   H.Cnt(elseC, Seq.empty[Symbol], transform(elseE)((a: H.Atom) => H.AppC(c, Seq(a))))),
+               args.foldRight(H.If(prim.asInstanceOf[L3TestPrimitive], Seq.empty[H.Atom], thenC, elseC))
+                             ((e, t) => transform(e)
+                                                 ((a: H.Atom) => {
+                                                   t match
+                                                     case H.If(cond, args, thenC, elseC) =>
+                                                       H.If(cond, a +: args, thenC, elseC)
+                                                     // Case match not exhaustive!
+                                                 })))
+                                                 
+      case S.If(cond: S.Tree, thenE: S.Tree, elseE: S.Tree) =>
+        transform(S.If(S.Prim(L3TestPrimitive.Eq,
+                              Seq(cond, S.Lit(CL3Literal.BooleanLit(false)))),
+                       elseE,
+                       thenE))
+                 (ctx)
+      case S.Prim(prim: L3TestPrimitive, args: Seq[S.Tree]) =>
+        transform(S.If(S.Prim(prim, args),
+                       S.Lit(CL3Literal.BooleanLit(true)),
+                       S.Lit(CL3Literal.BooleanLit(false))))
+                 (ctx)
+      /*
+      case S.Prim(prim, args) => 
+        ???
+      case S.Halt(arg: S.Tree) =>
+        ???
+      */
       case S.Ident(value) => 
         ctx(value)
       case S.Lit(value) => 
